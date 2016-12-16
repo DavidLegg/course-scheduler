b7c6a058ea9f0e01b714308e945736005b235ed1
Alex I. Ramirez <alex@arcompware.com>
2016-12-16 06:49:52 -0800

    Update classretrieval.php

--- classretrieval.php
+++ classretrieval.php
@@ -1,11 +1,10 @@
 <?php
 require_once('scheduler/controller.php');
-
-
-$courses = getCoursesByDept($_POST['option']);
+$courses = getCoursesByDept($_GET['option']);
+var_dump($_POST['option']);
+var_dump($courses);
 foreach($courses as $course){
   echo '<option value='.substr($course, 0,10).'>'.$course.'</option>';
 }
-
 
 ?>
