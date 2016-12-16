8c9832d97700107424f7cbb2a9dae25298b43a93
Alex I. Ramirez <alex@arcompware.com>
2016-12-16 05:07:10 -0800

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,5 +1,6 @@
 <?php
 ini_set('display_errors', 1);
+global $xpath, $baseQueryUrl;
 $baseUrl = 'https://www.reg.uci.edu/perl/WebSoc';
 $baseQueryUrl = 'https://www.reg.uci.edu/perl/WebSoc?ShowFinals=1&ShowComments=0';
 
@@ -8,17 +9,18 @@ $dom = new DOMDocument;
 $dom->loadHTML($html);
 $xpath = new DOMXPath($dom);
 
-var_dump($xpath);
 
 function getDropDownItems($name) {
   global $xpath;
+//   var_dump($name);
   $allOpts = $xpath->query('//select[@name="'.$name.'"]')[0];
+//   var_dump($allOpts);
   return $allOpts->getElementsByTagName("option");
 }
 
 function appendTerm(){
   global $baseQueryUrl;
-  $term = getDropDownItems('YearTerm');
+  $term = getDropDownItems('YearTerm')[0];
   $termId = $term->getAttribute('value');
   $baseQueryUrl .= '&YearTerm='.$termId;
 }
