d4ee6e4b091d0e1619db73bebfc3108eb20d3839
Alex I. Ramirez <alex@arcompware.com>
2016-12-16 04:45:21 -0800

    Create controller.php

---
+++ controller.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+$baseUrl = 'https://www.reg.uci.edu/perl/WebSoc';
+$baseQueryUrl = 'https://www.reg.uci.edu/perl/WebSoc?ShowFinals=1&ShowComments=0';
+
+$html = file_get_contents($baseUrl);
+$dom = new DOMDocument;
+$dom->loadHTML($html);
+$xpath = new DOMXPath($dom);
+
+function getDropDownItems($name) {
+  $allOpts = $xpath->query('//select[@name="'.$name.'"]')[0];
+  return $allOpts->getElementsByTagName("option");
+}
+
+function appendTerm(){
+  $term = getDropDownItems('YearTerm');
+  $termId = $term->getAttribute('value');
+  $baseQueryUrl .= '&YearTerm='.$termId;
+}
+
+appendTerm();
+
+
+
+?>
