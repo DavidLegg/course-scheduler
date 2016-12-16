f2667de2c349d4dc35698afb7c7209416e4dfc24
David Legg <davey.legg@gmail.com>
2016-12-15 21:44:49 -0800

    Untested. Added preference sort for schedules.

--- preferences.php
+++ preferences.php
@@ -7,7 +7,7 @@ class Preferences
 {
   protected $categories; // array(string => array(callable, float)) : array(name => array(category, weighting))
 
-  function __construct(array $preferenceCategories = array()) {
+  public function __construct(array $preferenceCategories = array()) {
     $this->$categories = array();
     foreach ($preferenceCategories as $prefCat) {
       if (array_key_exists($prefCat->$name, $this->$categories)) {
@@ -17,19 +17,28 @@ class Preferences
     }
   }
 
-  function changeWeight(string $categoryName, float $weight) {
+  public function changeWeight(string $categoryName, float $weight) {
     if (!array_key_exists($categoryName, $this->$categories)) {
       throw new Exception('Invalid category name');
     }
     $this->$categories[$categoryName][1] = $weight;
   }
 
-  function score(Schedule $schedule) {
+  public function score(Schedule $schedule) {
     $score = 0.0;
     foreach ($this->$categories as list($evaluate,$weight)) {
       $score += $evaluate($schedule) * $weight;
     }
     return $score;
+  }
+
+  public function sort(array $schedules) {
+    // make an array(schedule => score)
+    $schedToScore = array_combine($schedules, array_map(array($this, 'score'), $schedules));
+    // sort it by decreasing values, preserving keys
+    arsort($schedToScore);
+    // return the keys (schedules) sorted by their scores, descending.
+    return array_keys($schedToScore);
   }
 }
 
