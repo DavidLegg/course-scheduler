cb3efc78c288f06279b2f8b77aefa4f395a54f29
David Legg <davey.legg@gmail.com>
2016-12-16 23:41:37 -0800

    Testing increased...

--- main.php
+++ main.php
@@ -13,52 +13,54 @@ require_once ROOT_PATH.'schedule.php';
 require_once ROOT_PATH.'standardPreferences.php';
 
 echo "<br>All classes loaded correctly.<br>";
-// 
-// /**
-//  * A quick hack to load the test data.
-//  * A small amount of this code will be transferrable to classretrieval.
-//  */
-// function readCSV($file) {
-//   $handle = fopen($file, "r");
-//   if (!$handle) {
-//     throw new Exception("Could not open file.");
-//   }
 
-//   $courses = array(); //name => course
+/**
+ * A quick hack to load the test data.
+ * A small amount of this code will be transferrable to classretrieval.
+ */
+function readCSV($file) {
+  $handle = fopen($file, "r");
+  if (!$handle) {
+    throw new Exception("Could not open file.");
+  }
 
-//   $dayCodes = array(
-//     "M" => "monday",
-//     "Tu" => "tuesday",
-//     "W" => "wednesday",
-//     "Th" => "thursday",
-//     "F" => "friday"
-//   );
-//   while (($line = fgets($handle, 4096)) !== false) {
-//     list($course,$type,$shortDays,$times,$final,$code) = explode(',', $line);
+  $courses = array(); //name => course
+
+  $dayCodes = array(
+    "M" => "monday",
+    "Tu" => "tuesday",
+    "W" => "wednesday",
+    "Th" => "thursday",
+    "F" => "friday"
+  );
+  while (($line = fgets($handle, 4096)) !== false) {
+    list($course,$type,$shortDays,$times,$final,$code) = explode(',', $line);
     
-//     $days = array();
-//     foreach ($dayCodes as $short => $long) {
-//       $days[$long] = is_int(strpos($shortDays,$short)); //shortDays has short
-//     }
+    $days = array();
+    foreach ($dayCodes as $short => $long) {
+      $days[$long] = is_int(strpos($shortDays,$short)); //shortDays has short
+    }
 
-//     list($start,$end) = explode('-',$times);
-//     $startTime = new Time($start);
-//     $endTime   = new Time($end);
+    list($start,$end) = explode('-',$times);
+    $startTime = new Time($start);
+    $endTime   = new Time($end);
 
-//     $final = preg_replace("/-\d{1,2}:\d{2}/","",$final); //trim out ending time
-//     $finalDateTime = new DateTime($final);
+    $final = preg_replace("/-\d{1,2}:\d{2}/","",$final); //trim out ending time
+    $finalDateTime = new DateTime($final);
 
-//     if (!array_key_exists($course, $courses)) {
-//       $courses[$course] = new Course($course);
-//     }
-//     $courses[$course]->addClass(new Class(
-//       $days, $startTime, $endTime, $finalDateTime, $course, $type
-//     ));
-//   }
-//   fclose($handle);
+    if (!array_key_exists($course, $courses)) {
+      $courses[$course] = new Course($course);
+    }
+    $courses[$course]->addClass(new Class(
+      $days, $startTime, $endTime, $finalDateTime, $course, $type
+    ));
+  }
+  fclose($handle);
 
-//   return $courses;
-// }
+  return $courses;
+}
+
+echo "readCSV function defined.<br>";
 
 // $courses = readCSV("./testdata.csv");
 
