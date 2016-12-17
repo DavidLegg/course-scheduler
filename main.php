2059620331204a4816d803455738bfdf6da264a3
David Legg <davey.legg@gmail.com>
2016-12-16 17:04:47 -0800

    First test.

--- main.php
+++ main.php
@@ -1,7 +1,15 @@
 <?php
 
-// require_once('./controller.php');
-
 echo "Hello, world!<br>PHP is running correctly.";
+
+// require_once('./controller.php');
+require_once('./time.php');
+require_once('./class.php');
+require_once('./course.php');
+require_once('./schedule.php');
+require_once('./standardPreferences.php');
+
+
+
 
 ?>
