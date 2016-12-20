41bea73a3f7dfe370bdf8ef766432672a51ba9ed
Alex I. Ramirez <alex@arcompware.com>
2016-12-20 03:17:29 -0800

    Added flexibility

--- ui/coursesretrieval.php
+++ ui/coursesretrieval.php
@@ -1,13 +1,20 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once('../controller.php');
-$courses = getCoursesByDept($_GET['option']);
-//var_dump($_GET['option']);
-//var_dump($courses);
-foreach($courses as $course){
-//    echo $course;
-  echo '<option value="',$course->name,'">',$course->name,'</option>';
-}
-
-?>
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    require_once('../controller.php');
+    
+    switch($_POST['action']){
+        case 'coursebydept':
+            $courses = getCoursesByDept($_POST['option']);
+            //var_dump($_GET['option']);
+            //var_dump($courses);
+            foreach($courses as $course){
+                //    echo $course;
+                echo '<option value="',$course->name,'">',$course->name,'</option>';
+            }
+            break;
+        default:
+            break;
+    }
+    
+    ?>
