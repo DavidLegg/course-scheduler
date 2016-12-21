0662d4d9ea4b22d4320f4e1f0a7561b7c5fae57b
Alex I. Ramirez <alex@arcompware.com>
2016-12-21 03:48:33 -0800

    Warning added as comment

--- time.php
+++ time.php
@@ -23,6 +23,7 @@ class Time extends DateTime {
     throw new Exception("Cannot set Date in a Time object.");
   }
 
+// Strict Standards: Declaration of Time::add() should be compatible with DateTime::add($interval)
   public function add(DateInterval $interval) {
     parent::add($interval);
 //    $this->setDate(2000,1,1); // just rollover extra time. (Commented to execute the rest) 
