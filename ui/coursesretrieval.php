0c8dd82fd8e1f9c5696d0cb4687bd97bb0c8a958
Alex I. Ramirez <alex@arcompware.com>
2016-12-21 03:48:50 -0800

    idk what was wrong but it fixed it

--- ui/coursesretrieval.php
+++ ui/coursesretrieval.php
@@ -1,38 +1,68 @@
 <?php
+    
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     require_once('../controller.php');
     session_start();
-//    //global $addedCourses;
-//    
-//    echo "LOL";
-    var_dump( $_GET['_param']);
-    echo "<br/>";
-    switch($_GET['_action']){
-        case 'coursebydept':
-            $courses = getCoursesByDept($_GET['_param']);
-            //var_dump($_GET['option']);
-            //var_dump($courses);
-            foreach($courses as $name => $course){
-                //    echo $course;
-                echo '<option value="',$name,'">',$name,'</option>';
-            }
-            break;
-        case 'addcourse':
-//            var_dump($_SESSION['addedCourses']);
-            $_SESSION['addedCourses'][$_GET['_param']] = $_SESSION['availableCourses'][$_GET['_param']];
-            foreach($_SESSION['addedCourses'] as $name => $course){
-                echo '<li id="',$name,'">',$name,'</li>';
-            }
-            break;
-        case 'schedule':
-            $schedules = generateSchedules();
-            foreach ($schedules as $sched) {
-                echo $sched,"<hr>";
-            }
-            break;
-        default:
-            break;
-    }
     
+    if (isset($_GET['_action'])){
+//        if ($_GET['_action'] == 'coursebydept')
+//        {
+//            $courses = getCoursesByDept($_GET['_param']);
+//            foreach($courses as $name => $course){
+//                echo '<option value="',$name,'">',$name,'</option>';
+//            }
+//        }
+//        else if ($_GET['_action'] == 'addcourse')
+//        {
+//            $_SESSION['addedCourses'][$_GET['_param']] = $_SESSION['availableCourses'][$_GET['_param']];
+//            foreach($_SESSION['addedCourses'] as $name => $course){
+//                echo '<li id="',$name,'" style="color:#0039ad;">',$name,'</li>';
+//            }
+//        }
+//        else if ($_GET['_action'] == 'schedule')
+//        {
+//            $schedules = generateSchedules();
+//            echo "Course Selection: ";
+//            $courseList = '';
+//            foreach ($_SESSION['addedCourses'] as $name => $course){
+//                $courseList .= $name . ", ";
+//            }
+//            echo '<span style="color:#0039ad;">',substr($courseList, 0, -2), "</span><br/>";
+//            foreach ($schedules as $sched) {
+//                echo $sched,"<hr>";
+//            }
+//        }
+
+        switch($_GET['_action']){
+            case 'coursebydept':
+                $courses = getCoursesByDept($_GET['_param']);
+//                var_dump($courses);
+                foreach($courses as $name => $course){
+                    echo '<option value="',$name,'">',$course->name,'</option>';
+                }
+                var_dump($_SESSION['availableCourses']);
+                break;
+            case 'addcourse':
+                $_SESSION['addedCourses'][$_GET['_param']] = $_SESSION['availableCourses'][$_GET['_param']];
+                foreach($_SESSION['addedCourses'] as $name => $course){
+                    echo '<li id="',$name,'" style="color:#0039ad;">',$course->name,'</li>';
+                }
+                break;
+            case 'schedule':
+                $schedules = generateSchedules();
+                echo "Course Selection: ";
+                $courseList = '';
+                foreach ($_SESSION['addedCourses'] as $course){
+                    $courseList .= $course->name . ", ";
+                }
+                echo '<span style="color:#0039ad;">',substr($courseList, 0, -2), "</span><br/>";
+                foreach ($schedules as $sched) {
+                    echo $sched,"<hr>";
+                }
+                break;
+            default:
+                break;
+        }
+    }
     ?>
