f169c18ee7c5e11261b5019e42d5cc1f1aab79c0
Alex I. Ramirez <alex@arcompware.com>
2017-02-13 23:37:07 -0800

    php greatness

--- site/index.php
+++ site/index.php
@@ -1,3 +1,14 @@
+<?php
+    session_start();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+//    phpinfo();
+    $_SESSION['availableCourses'] = array();
+    $_SESSION['addedCourses'] = array();
+    require_once('scheduler/controller.php');
+    
+    
+?>
 <html>
 
 	<head>
