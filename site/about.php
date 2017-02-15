49db0822f516c26a9862af0de4beed61b80c6f96
Alex I. Ramirez <alex@arcompware.com>
2017-02-14 23:56:44 -0800

    Responsiveness, Help link

--- site/about.php
+++ site/about.php
@@ -3,6 +3,8 @@
 	<head>
 		<title>ZotScheduler</title>
 <meta name="theme-color" content="#eeee22">
+<meta name="viewport" content="width=device-width">
+
 		<link rel="stylesheet" type="text/css" href="style.css">
 <link rel="stylesheet" id="screenr-fonts-css" href="https://fonts.googleapis.com/css?family=Open%2BSans%3A400%2C300%2C300italic%2C400italic%2C600%2C600italic%2C700%2C700italic%7CMontserrat%3A400%2C700&amp;subset=latin%2Clatin-ext" type="text/css" media="all">
 	</head>
@@ -15,6 +17,7 @@
 					<ul id="nav">
 						<li><a href="index.php">Home</a></li>
 						<li><a href="about.php">About</a></li>
+                        <li><a href="index.php#help">Help</a></li>
 					</ul>
 				</div>
 			</div>
